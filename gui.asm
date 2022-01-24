c3337ee299da0729f8dfd1219e77499bebbd162e
KlimKlimakov <93843835+KlimKlimakov@users.noreply.github.com>
2022-01-24 21:54:35 +0300

    Add files via upload

---
+++ gui.asm
@@ -0,0 +1,22 @@
+format  PE GUI
+entry   start
+include 'include\win32a.inc'
+
+ section '.data' data readable writeable
+
+  text db  'Failed', 0
+  text2 db 'You are okay?', 0
+
+ section '.code' code readable executable
+
+ start:
+         invoke  MessageBox,NULL,text,text2,MB_OK
+         invoke  ExitProcess, 0
+
+ section '.idata' import data readable writeable
+         library kernel32,'kernel32.dll',\
+                 user32,'user32.dll'
+         import  kernel32,\
+                 ExitProcess,'ExitProcess'
+         import  user32,\
+                 MessageBox,'MessageBoxA'
